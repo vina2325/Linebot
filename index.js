f810487c8e2521bc7241646c46f000bbc688f70f
vina2325 <1104163140@nkust.edu.tw>
2020-05-24 21:25:38 +0800

    縮短搜尋範圍(從方圓1公里改成方圓500公尺)

--- index.js
+++ index.js
@@ -47,12 +47,19 @@ bot.on('message', async (event) => {
   try {
     if (event.message.type === 'location') {
       const data = await rp({
-        uri: `https://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-000467?filters=Longitude%20gt%20%27${(event.message.longitude) - 0.0103637682661416}%27%20and%20Longitude%20lt%20%27${(event.message.longitude) + 0.0103637682661416}%27%20and%20Latitude%20lt%20%27${(event.message.latitude) + 0.009021199819576}%27%20and%20Latitude%20gt%20%27${(event.message.latitude) - 0.009021199819576}%27&offset=0&limit=1000`,
+        uri: `https://opendata.epa.gov.tw/webapi/api/rest/datastore/355000000I-000467?filters=Longitude%20gt%20%27${(event.message.longitude) - 0.0051818841330708}%27%20and%20Longitude%20lt%20%27${(event.message.longitude) + 0.0051818841330708}%27%20and%20Latitude%20lt%20%27${(event.message.latitude) + 0.004510599909788}%27%20and%20Latitude%20gt%20%27${(event.message.latitude) - 0.004510599909788}%27&offset=0&limit=1000`,
         json: true
       })
       for (let i = 0; i < data.result.records.length; i++) {
         msg += '🚾' + data.result.records[i].Name + '\n' + '➡' + data.result.records[i].Address + '\n' + '🈴' + data.result.records[i].Grade + '\n' + ' ' + '\n'
-        if (i === 30) { break }
+        // msg2.push({
+        //   type: 'location',
+        //   title: data.result.records[0].Name,
+        //   address: data.result.records[0].Address,
+        //   latitude: data.result.records[0].Latitude,
+        //   longitude: data.result.records[0].Longitude
+        // })
+        if (i >= 9) { break }
       }
 
       msg += '〰〰〰〰〰〰〰〰〰〰〰〰〰' + '\n' + '✍請輸入欲前往【廁所名稱】'
